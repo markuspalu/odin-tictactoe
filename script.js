a7eec0b7fea2c51b5a1e95f2bc59540afbc9bab6
markuspalu <palu1@stolaf.edu>
2023-07-22 02:02:08 +0300

    working ugly code

--- script.js
+++ script.js
@@ -24,7 +24,6 @@ const Gameboard = (() => {
 
     const restartGame = () => {
         console.log("restarting game");
-        const boxes = document.querySelectorAll(".box");
         Gameboard.gameArray = [
         "", "", "",
         "", "", "",
@@ -41,7 +40,6 @@ const Gameboard = (() => {
 
 const Player = (name, mark) => {
     let currentPlayer = null;
-
     let counter = 0;
     const getName = () => console.log("name is " + name);
     const getCounter = () => console.log("current counter is "+ counter);
@@ -55,20 +53,26 @@ const Player = (name, mark) => {
             updateCounter2()
         }
         
+        setTimeout(() => {
+            if (player1.counter == 3) {
+                winGame(player1);
+            } else if (player2.counter == 3) {
+                winGame(player2);
+            };
+        }, 2000);
+
+        
         Gameboard.restartGame();
-        if (player1.counter == 3 || player2.counter == 3) {
-            winGame();
+    };
+
+    const winGame = (player) => {
+        screen2.style.display = "none";
+        screen3.style.display = "flex";
+        if (player == "tie") {
+            whoWon.innerHTML = "It is a tie between " + player1.name + " and " + player2.name + "!";
+        } else {
+            whoWon.innerHTML = "Congratulations " + player.name + "!!!";
         }
-    };
-
-    const winGame = () => {
-        console.log("winner winner!");
-        alert("Games are over!")
-    };
-
-    const takeTurn = (index, mark) => {
-        console.log("start takeTurn");
-        makeMove(index, mark);
     };
 
     const checkTie = () => {
@@ -76,25 +80,38 @@ const Player = (name, mark) => {
             player1.counter++;
             player2.counter++;
             boxes.forEach(box => box.classList.add('disabled'));
+    
             setTimeout(() => {
-                Gameboard.restartGame();
                 updateCounter1()
                 updateCounter2()
+                Gameboard.restartGame();
                 boxes.forEach(box => box.classList.remove('disabled'));
+                if (player1.counter == 3 && player2.counter == 3) {
+                    setTimeout(() => {
+                        winGame("tie");
+                    }, 2000);
+                } else {
+                    if (player1.counter == 3) {
+                        setTimeout(() => {
+                            winGame(player1);
+                        }, 2000);
+                    } else if (player2.counter == 3) {
+                        setTimeout(() => {
+                            winGame(player2);
+                        }, 2000);
+                    };
+                }
+
               }, 2000);
-              
-        };
-
-            if (player1.counter == 3 && player2.counter == 3) {
-                alert("it's a tie! Game over!");
             }
-    }    
+        };
+      
 
-    const boxes = document.querySelectorAll(".box");
     const makeMove = (index, mark) => {
         Gameboard.gameArray[index] = mark;
         boxes[index].innerHTML = mark;
         checkTie();
+        
         if (Gameboard.checkArray(mark) == true) {
             boxes.forEach(box => box.classList.add('disabled'));
             setTimeout(() => {
@@ -110,13 +127,12 @@ const Player = (name, mark) => {
             currentPlayer = mark === "X" ? player1 : player2;
         }
         return new Promise((resolve) => {
-            const boxes = document.querySelectorAll(".box");
             const boxClickHandler = (event) => {
                 const box = event.target;
 
                 
                 if (Gameboard.gameArray[box.id.slice(-1)] === "") {
-                    takeTurn(box.id.slice(-1), mark);
+                    makeMove(box.id.slice(-1), mark);
                     currentPlayer = currentPlayer === player1 ? player2 : player1;
                     resolve();
                     boxes.forEach(box => box.removeEventListener('click', boxClickHandler));
@@ -128,59 +144,67 @@ const Player = (name, mark) => {
                 });
 
             });
-            // Reminder:
-            // 1. We run finalMove()
-            // 2. We set addEventListener to every box, with click to activate boxClickHandler
-            // 3. When boxClickHandler finishes running, we removeEventListener from ALL boxes
-            
         };
 
-    return {name, mark, getName, getCounter, winOne, winGame, takeTurn, finalMove, makeMove, counter};
+    return {name, mark, getName, getCounter, winOne, winGame, finalMove, makeMove, counter};
 };
 
 let player1;
 let player2;
 
+const boxes = document.querySelectorAll(".box");
 const submitButton = document.querySelector(".submitButton");
 const name1 = document.getElementById("name1");
 const name2 = document.getElementById("name2");
 const screen1 = document.querySelector(".screen1");
 const screen2 = document.querySelector(".screen2");
+const screen3 = document.querySelector(".screen3");
+const whoWon = document.querySelector(".whoWon");
+const maincontainer2 = document.querySelector(".maincontainer2");
+const player1class = document.querySelector(".player1");
+const player2class = document.querySelector(".player2");
+const playerName1 = document.querySelector(".playerName1");
+const playerName2 = document.querySelector(".playerName2");
+const playerCounter1 = document.querySelector(".playerCounter1");
+const playerCounter2 = document.querySelector(".playerCounter2");
+
 submitButton.addEventListener("click", () => {
     if (name1.value != "" && name2.value != "") {
         player1 = Player(name1.value, "X");
         player2 = Player(name2.value, "O");
         screen1.style.display = "none";
         screen2.style.display = "flex";
+
+        maincontainer2.classList.add("zoom-in2");
+        player1class.classList.add("zoom-left");
+        player2class.classList.add("zoom-right");
+        setTimeout(() => {
+          maincontainer2.classList.remove("zoom-in2");
+          player1class.classList.remove("zoom-left");
+          player2class.classList.remove("zoom-right");
+        }, 1000);
         playGames()
         runSecondScreen()
     }
 })
 
-const playerName1 = document.querySelector(".playerName1");
-const playerName2 = document.querySelector(".playerName2");
-const playerCounter1 = document.querySelector(".playerCounter1");
-const playerCounter2 = document.querySelector(".playerCounter2");
 const runSecondScreen = () => {
     playerName1.innerHTML = player1.name;
     playerName2.innerHTML = player2.name;
 
 }
 
-
 const updateCounter1 = () => {
     playerCounter1.innerHTML = player1.counter;
     playerCounter1.classList.add("zoom-in");
-    // Remove the "zoom-in" class after the animation completes (1s)
     setTimeout(() => {
       playerCounter1.classList.remove("zoom-in");
     }, 1000);
 }
 
 const updateCounter2 = () => {
-    playerCounter2.innerHTML = player1.counter;
+    playerCounter2.innerHTML = player2.counter;
     playerCounter2.classList.add("zoom-in");
-    // Remove the "zoom-in" class after the animation completes (1s)
     setTimeout(() => {
       playerCounter2.classList.remove("zoom-in");
     }, 1000);
@@ -202,3 +226,4 @@ const playGames = async() => {
         await startGame();
     }
 }
+
